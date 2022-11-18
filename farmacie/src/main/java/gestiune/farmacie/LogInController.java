a3cbbe65e027b6b581e3e6b70bc812cd7ce3524a
xBisharp <raduletuhoria@gmail.com>
2022-11-18 10:30:07 +0200

    Pagina SignIn/LogIn

---
+++ farmacie/src/main/java/gestiune/farmacie/LogInController.java
@@ -0,0 +1,18 @@
+package gestiune.farmacie;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LogInController {
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+}
---
+++ farmacie/src/main/java/gestiune/farmacie/SignInController.java
@@ -0,0 +1,17 @@
+package gestiune.farmacie;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignInController {
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private PasswordField confirmField;
+    @FXML
+    private Button signButton;
+}

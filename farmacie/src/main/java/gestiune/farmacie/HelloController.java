87d4d507adc1245c551238c0fa489d8ec4f72c4f
xBisharp <raduletuhoria@gmail.com>
2022-11-18 08:51:45 +0200

    test test

--- farmacie/src/main/java/gestiune/farmacie/HelloController.java
+++ farmacie/src/main/java/gestiune/farmacie/HelloController.java
@@ -4,11 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+@FXML
+private Label mylabel;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+  public void testTest(){
+      mylabel.setText("Test test !");
+  }
 }

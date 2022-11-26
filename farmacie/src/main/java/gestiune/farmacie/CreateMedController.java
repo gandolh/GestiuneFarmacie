638ec3a92974f48d1536c6ce2bd163309d7a1472
xBisharp <raduletuhoria@gmail.com>
2022-11-26 20:05:47 +0200

    interfata update

---
+++ farmacie/src/main/java/gestiune/farmacie/CreateMedController.java
@@ -0,0 +1,37 @@
+package gestiune.farmacie;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class CreateMedController {
+
+    @FXML
+    private ComboBox dropMed;
+
+    @FXML
+    private ComboBox dropFurnizor;
+
+    @FXML
+    private TextField textPret;
+
+    @FXML
+    private TextField textStoc;
+
+    @FXML
+    private TextField textComentarii;
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    private  Button buttonCancel;
+
+    @FXML
+    private Button buttonPlus;
+
+    @FXML
+    private Button buttonMinus;
+
+}
